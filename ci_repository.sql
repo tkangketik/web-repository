0fac5fd1da1726fe25ba310ea2b157a1a101a3fa
umarhadi <hi@umarhadi.dev>
2022-06-19 19:45:10 +0700

    db: remove unused tables
    Signed-off-by: umarhadi <hi@umarhadi.dev>

--- ci_repository.sql
+++ ci_repository.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jun 19, 2022 at 10:59 AM
+-- Generation Time: Jun 19, 2022 at 12:44 PM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -20,25 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `ci_repository`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `authors`
---
-
-CREATE TABLE `authors` (
-  `id` int(10) NOT NULL,
-  `author` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `authors`
---
-
-INSERT INTO `authors` (`id`, `author`) VALUES
-(1, 'umar'),
-(2, 'toif');
 
 -- --------------------------------------------------------
 
@@ -61,7 +42,9 @@ CREATE TABLE `enkripsi` (
 INSERT INTO `enkripsi` (`id`, `username`, `hasil`, `kunci`, `tgl`) VALUES
 (2, 'umar', 'kasodkasodk', 'kkkkk', '2022-06-19'),
 (4, 'umar', 'DZNNZRZNZ', '1', '2022-06-19'),
-(5, 'admin', 'DZNNZRZNZDZNNZRZNZDZNNZRZNZ', '696969696', '2022-06-19');
+(5, 'admin', 'DZNNZRZNZDZNNZRZNZDZNNZRZNZ', '696969696', '2022-06-19'),
+(6, 'admin', 'LNDQKLX', '12345', '2022-06-19'),
+(8, 'bagas00', 'LNDQSDXV', '12345', '2022-06-19');
 
 -- --------------------------------------------------------
 
@@ -111,30 +94,11 @@ CREATE TABLE `repo` (
 INSERT INTO `repo` (`id`, `date`, `year`, `id_author`, `username`, `id_type`, `id_prodi`, `title`, `description`, `file_repo`, `thumbnail`, `nama`, `tipe`) VALUES
 (5, '3333-03-31', 3333, 0, '', 0, 2, 'ads', '', '', '7a7de2a2c392e8f4d3e5e8e7d4d7a0494f836d6f.png', 'awkoawko', 'akwokaow'),
 (6, '2222-02-22', 2222, 0, '', 0, 1, 'adasdd', '', '', '2612470572.png', 'ini nama', 'ini tipe'),
-(8, '2222-02-22', 2222, 0, '', 0, 2, 'tessttt', '', '', '1.png', 'bukan umar', 'png'),
+(8, '2222-02-22', 2222, 0, '', 0, 2, 'tessttt', '', '', '1.png', 'umar asli', 'png'),
 (9, '0002-02-22', 2, 0, '', 0, 1, 'adawadwdaw', '', '', '7a7de2a2c392e8f4d3e5e8e7d4d7a0494f836d6f1.png', 'umaaarr', 'png'),
 (12, '2222-02-22', 2222, 0, 'umar', 0, 1, 'sadasasd', '', '', 'parker-hilton-vFzZ1_LPttk-unsplash.jpg', 'kokkokokokokokoko', 'klklklklkl'),
 (14, '2022-06-14', 2022, 0, 'bagas00', 0, 1, 'ikan', '', '', 'WhatsApp_Image_2022-05-25_at_14.41.441.jpeg', 'Yuliana', 'jpg'),
 (15, '2022-06-14', 2022, 0, 'bagas00', 0, 2, 'ikan', '', '', 'cover_(19).png', 'Yuliana', 'jpg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `types`
---
-
-CREATE TABLE `types` (
-  `id` int(10) NOT NULL,
-  `type` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `types`
---
-
-INSERT INTO `types` (`id`, `type`) VALUES
-(1, 'tip'),
-(2, 'png');
 
 -- --------------------------------------------------------
 
@@ -163,17 +127,12 @@ INSERT INTO `users` (`id`, `nama`, `alamat`, `email`, `username`, `password`, `l
 (7, 'Umar hadi Siswanto', 'disanaa', 'hi@umarhadi.dev', 'hehehe', '69492767a592621ec0e785e0135e1c6f', 'admin'),
 (8, 'TOIFUDDIN ROZIKI', 'jakal', 'toifuddinroziki@gmail.com', 'Toif', '83dbb2993441f2eaa9abab51a19cb693', 'admin'),
 (9, 'Yuliana', 'jalan Jaten', 'yulianaroiz02@gmail.com', 'yuliana12', 'f2c4416ef8c5a6b48d952286d3b9e79d', 'admin'),
-(10, 'Bagas', 'jalan kaliurang', 'bagas12@gmail.com', 'bagas00', '5ffd9bb73b00bce4feeb77e2d12722da', 'admin');
+(10, 'Bagas', 'jalan kaliurang', 'bagas12@gmail.com', 'bagas00', '5ffd9bb73b00bce4feeb77e2d12722da', 'admin'),
+(11, 'yulianto aja', 'cibitung', 'yulianto@gmail.com', 'yuliantok', '7b5adea9f129b861e3291e851a9e15e9', 'admin');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `authors`
---
-ALTER TABLE `authors`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `enkripsi`
@@ -194,12 +153,6 @@ ALTER TABLE `repo`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `types`
---
-ALTER TABLE `types`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -210,16 +163,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `authors`
---
-ALTER TABLE `authors`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
 -- AUTO_INCREMENT for table `enkripsi`
 --
 ALTER TABLE `enkripsi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `prodi`
@@ -234,16 +181,10 @@ ALTER TABLE `repo`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT for table `types`
---
-ALTER TABLE `types`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
